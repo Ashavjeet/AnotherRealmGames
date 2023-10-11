23928dbe984e86f8d1f6d1c12c676438de5e77c9
Ashavjeet <ashus7053@gmail.com>
2023-10-11 21:29:35 +1300

    Initial Commit

---
+++ WEVManukauTech/scripts/RandomPage.js
@@ -0,0 +1,17 @@
+﻿var links = new Array(10) 
+links[0] = "https://anotherrealmgames.azurewebsites.net/Ps4_Godofwar.html"
+links[1] = "http://anotherrealmgames.azurewebsites.net/Xbox_asc.html"
+links[2] = "http://anotherrealmgames.azurewebsites.net/Nin_border.html"
+links[3] = "http://anotherrealmgames.azurewebsites.net/Win_witcher.html"
+links[4] = "http://anotherrealmgames.azurewebsites.net/Win_ant.html"
+links[5] = "http://anotherrealmgames.azurewebsites.net/Win_sniper.html"
+links[6] = "http://anotherrealmgames.azurewebsites.net/Nin_mk.html"
+links[7] = "http://anotherrealmgames.azurewebsites.net/Ps4_Infamous.html"
+links[8] = "http://anotherrealmgames.azurewebsites.net/Win_doom.html"
+links[9] = "http://anotherrealmgames.azurewebsites.net/Xbox_cyber.html"
+links[10] = "http://anotherrealmgames.azurewebsites.net/Ps4_Spiderman.html"
+function go() {
+    var a = Math.floor(Math.random() * links.length);
+    var i = a
+    location = links[i]
+}
---
+++ WEVManukauTech/scripts/XSiteControls.js
@@ -0,0 +1,7 @@
+﻿window.onload = function () {
+    var xhttp = new XMLHttpRequest();
+    var oeMenu = document.getElementById("menu");
+    xhttp.open("GET", "sharedMenu.html", false);
+    xhttp.send();
+    oeMenu.innerHTML = xhttp.responseText;
+}
